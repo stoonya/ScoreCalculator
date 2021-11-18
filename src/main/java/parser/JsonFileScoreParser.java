5c324c0f1db0f477e0d6ddfd07e66ea75ee0f87f
stoonya <stasenko.s.j@gmail.com>
2021-11-17 22:45:35 -0800

    fixed conflicts

--- src/main/java/parser/JsonFileScoreParser.java
+++ src/main/java/parser/JsonFileScoreParser.java
@@ -1,43 +1,37 @@
 package parser;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import validator.Validator;
+import models.ScoreModel;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class JsonFileParser implements Parser {
+public class JsonFileScoreParser implements ScoreParser {
 
     private ObjectMapper objectMapper;
     private String path;
-    private Validator validator;
 
-<<<<<<< Updated upstream:src/main/java/parser/JsonFileParser.java
-    public JsonFileParser(String path, Validator validator) {
-        this.objectMapper = new ObjectMapper();
-        this.validator = validator;
-=======
     public JsonFileScoreParser(String path) {
         // Assumption: Extra fields from JSON are ignored (fields that are not "id", "ip" and "score")
         this.objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);;
->>>>>>> Stashed changes:src/main/java/parser/JsonFileScoreParser.java
         this.path = path;
     }
 
-    public <T> List<T> parse(Class<T> classType) {
+    public List<ScoreModel> parse() {
 
-        List<T> jsonObjects = null;
+        List<ScoreModel> jsonObjects = null;
 
         try {
-            JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, classType);
+            JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, ScoreModel.class);
             jsonObjects =  objectMapper.readValue(new File(path), type);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return validator.filter(jsonObjects);
+        return jsonObjects;
     }
 }
--- src/main/java/validator/ScoreObjectsFilter.java
+++ src/main/java/validator/ScoreObjectsFilter.java
@@ -1,30 +1,26 @@
 package validator;
 
-import dto.ScoreDTO;
+import com.google.common.net.InetAddresses;
+import models.ScoreModel;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ScoreObjectsFilter implements Validator<ScoreDTO> {
+public class ScoreObjectsFilter implements Validator<ScoreModel> {
 
-    public List<ScoreDTO> filter(List<ScoreDTO> list) {
+    public List<ScoreModel> filter(List<ScoreModel> list) {
         return list.stream().filter(o -> isValidObject(o)).collect(Collectors.toList());
     }
 
     // Assumption: If ip is null or not in valid format (IPv4 or IPv6), skip the entire result object
     private boolean isValidIp(String ip) {
-        return ip != null && ip.matches("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+        return ip != null && InetAddresses.isInetAddress(ip);
     }
 
-<<<<<<< Updated upstream
-    // assumption #3: if score is null or not valid, skip the entire result object
-    // assumption #4: valid score means positive integer
-=======
     // Assumption: Score is an integer
     // Assumption: If score is null or less than 0, skip the entire result object
->>>>>>> Stashed changes
     private boolean isValidScore(int score) {
-        return score > 0;
+        return score >= 0;
     }
 
     // Assumption: If id is null, skip the entire result object
@@ -32,7 +28,7 @@ public class ScoreObjectsFilter implements Validator<ScoreDTO> {
         return id != null;
     }
 
-    private boolean isValidObject(ScoreDTO object) {
-        return isValidId(object.id) && isValidScore(object.score) && isValidIp(object.ip);
+    private boolean isValidObject(ScoreModel object) {
+        return isValidId(object.getId()) && isValidScore(object.getScore()) && isValidIp(object.getIp());
     }
 }
